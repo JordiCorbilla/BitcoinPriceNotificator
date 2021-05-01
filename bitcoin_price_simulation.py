efc4b10f784fd704fc5985b0f812614341fa4e58
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-05-01 21:22:54 +0100

    Create bitcoin_price_simulation.py

---
+++ bitcoin_price_simulation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 19:16:32 2021
+
+@author: thund
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+mu = 0.004
+sigma = 0.01
+start_price = 5
+np.random.seed(0)
+returns = np.random.normal(loc=mu, scale=sigma, size=100)
+price = start_price*(1+returns).cumprod()
+plt.plot(price)
